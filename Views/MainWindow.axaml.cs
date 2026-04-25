f168e96d0164a6a25fbee749f2f15bc401b99e0b
Akbar-26122005 <nurmatovakbar59@gmail.com>
2026-04-25 13:12:58 +0500

    Добавьте файлы проекта.

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace proect.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
