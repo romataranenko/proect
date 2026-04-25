60f43e972a12c0ef08d77c61932a5652a47e01ab
Akbar-26122005 <nurmatovakbar59@gmail.com>
2026-04-25 14:06:19 +0500

    fffb

---
+++ Data/DatabaseHelper.cs
@@ -0,0 +1,42 @@
+﻿using Npgsql;
+using System.Collections.Generic;
+
+using Npgsql;
+
+public class DatabaseHelper
+{
+    private static string connectionString =
+        "Host=localhost;Port=5432;Username=postgres;Password=root;Database=p511_db";
+
+    public static NpgsqlConnection GetConnection()
+    {
+        return new NpgsqlConnection(connectionString);
+    }
+
+    public List<TaskItem> LoadTasks()
+    {
+        var tasks = new List<TaskItem>();
+        using (var conn = GetConnection())
+        {
+            conn.Open();
+            var query = "SELECT * FROM tasks ORDER BY created_date DESC";
+            using (var cmd = new NpgsqlCommand(query, conn))
+            using (var reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    tasks.Add(new TaskItem
+                    {
+                        Id = reader.GetInt32(0),
+                        Title = reader.GetString(1),
+                        IsCompleted = reader.GetBoolean(2),
+                        Description = reader.IsDBNull(3) ? "" : reader.GetString(3),
+                        Priority = reader.IsDBNull(4) ? 2 : reader.GetInt32(4),
+                        CreatedDate = reader.GetDateTime(5)
+                    });
+                }
+            }
+        }
+        return tasks;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+public partial class MainForm : Form
+{
+    private DatabaseHelper dbHelper;
+
+    public MainForm()
+    {
+        InitializeComponent();
+        dbHelper = new DatabaseHelper();
+        LoadTasksToGrid();
+    }
+
+    private void LoadTasksToGrid()
+    {
+        dgvTasks.DataSource = dbHelper.LoadTasks();
+    }
+
+    private void btnAdd_Click(object sender, EventArgs e)
+    {
+        if (!string.IsNullOrWhiteSpace(txtTaskTitle.Text))
+        {
+            dbHelper.AddTask(txtTaskTitle.Text, txtDescription.Text, (int)nudPriority.Value);
+            LoadTasksToGrid();
+            txtTaskTitle.Clear();
+            txtDescription.Clear();
+            nudPriority.Value = 2;
+        }
+        else
+        {
+            MessageBox.Show("Введите название задачи!");
+        }
+    }
+
+    private void btnMarkCompleted_Click(object sender, EventArgs e)
+    {
+        if (dgvTasks.SelectedRows.Count > 0)
+        {
+            int taskId = (int)dgvTasks.SelectedRows[0].Cells["Id"].Value;
+            dbHelper.MarkAsCompleted(taskId);
+            LoadTasksToGrid();
+        }
+    }
+
+    private void btnDelete_Click(object sender, EventArgs e)
+    {
+        if (dgvTasks.SelectedRows.Count > 0)
+        {
+            int taskId = (int)dgvTasks.SelectedRows[0].Cells["Id"].Value;
+            dbHelper.DeleteTask(taskId);
+            LoadTasksToGrid();
+        }
+    }
+}
