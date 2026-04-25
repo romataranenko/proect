b44986ad682282a4f1d5224691fdbbde27292fe2
Akbar-26122005 <nurmatovakbar59@gmail.com>
2026-04-25 13:46:36 +0500

    rr

---
+++ Models/TaskItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class TaskItem
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public bool IsCompleted { get; set; }
+    public string Description { get; set; }
+    public int Priority { get; set; } 
+    public DateTime CreatedDate { get; set; }
+}
+
