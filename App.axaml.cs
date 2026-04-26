069209f9e3df5b2400544ef01e0e526ac4e3ed6d
Akbar-26122005 <nurmatovakbar59@gmail.com>
2026-04-26 14:22:27 +0500

    м

--- App.axaml.cs
+++ App.axaml.cs
@@ -2,7 +2,6 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using Microsoft.Extensions.DependencyInjection;
-using proect.Views;
 using TaskManager.Interfaces;
 using TaskManager.Services;
 using TaskManager.ViewModels;
